e4daf0c3fc8bccc1211402e4608be7d8e57b5d59
Cosmin Stoian <cosmyn9708@gmail.com>
2021-09-24 12:59:18 +0300

    Added ant.

---
+++ src/components/input/TextWidget.vue
@@ -0,0 +1,38 @@
+<template>
+    <a-input :value="value" @input="emitInput"></a-input>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "TextWidget",
+    props: {
+        value: {
+            type: String,
+            default: null,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            default: null,
+        },
+        readonly: {
+            type: Boolean,
+            default: false,
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data() {},
+    methods: {
+        emitInput($event) {
+            this.$emit("input", $event);
+        },
+    },
+});
+</script>
